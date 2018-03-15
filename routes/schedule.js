8fcbfd897885771205ffd079652109af99b8679a
Longinos Guizar <lguizar@ucsd.edu>
2018-03-14 17:56:32 -0700

    fixed schedule delete glitch

--- routes/schedule.js
+++ routes/schedule.js
@@ -6,15 +6,18 @@ var data = require('../data_event.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	var newSchedule = {
-		"name": req.query.name,
-		"time": req.query.time,
-		"location": req.query.location
-	};
+	if (req.query.name == undefined) {
+		  res.render('schedule', data);
+	} else {
+		var newSchedule = {
+			"name": req.query.name,
+			"time": req.query.time,
+			"location": req.query.location
+		};
 
-	data.events.push(newSchedule);
-    res.render('schedule', data);
-      	
+		data.events.push(newSchedule);
+	    res.render('schedule', data);
+
+	}
+
 };
-
-
